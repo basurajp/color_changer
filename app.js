af0dd3c3689ba9ab9220dfe08b1c8dab5711c5e1
Basuraj <basurajp135@gmail.com>
2023-08-19 23:49:33 +0530

    color change app done

---
+++ app.js
@@ -0,0 +1,28 @@
+const buttons = document.querySelectorAll(".buttons");
+
+const body = document.querySelector("body");
+console.log(buttons)
+
+buttons.forEach(function (button) {
+  console.log(button);
+  button.addEventListener("click", function (e) {
+    console.log(e);
+    console.log(e.target);
+
+    if (e.target.id === "grey") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "red") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "blue") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "yellow") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "purple") {
+      body.style.backgroundColor = e.target.id;
+    }
+  });
+});
