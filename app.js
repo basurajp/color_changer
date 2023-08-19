dfc9c6de44ff4eef9c37f209b85f0fc086869ead
Basuraj <basurajp135@gmail.com>
2023-08-20 00:01:07 +0530

    done whithout any refernce

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
-const buttons = document.querySelectorAll(".buttons");
+const buttons =document.querySelectorAll('.buttons');
 
-const body = document.querySelector("body");
-console.log(buttons)
+const body = document.querySelector('body');
 
-buttons.forEach(function (button) {
-  console.log(button);
-  button.addEventListener("click", function (e) {
-    console.log(e);
-    console.log(e.target);
+console.log(buttons);
 
-    if (e.target.id === "grey") {
-      body.style.backgroundColor = e.target.id;
-    }
-    if (e.target.id === "red") {
-      body.style.backgroundColor = e.target.id;
-    }
-    if (e.target.id === "blue") {
-      body.style.backgroundColor = e.target.id;
-    }
-    if (e.target.id === "yellow") {
-      body.style.backgroundColor = e.target.id;
-    }
-    if (e.target.id === "purple") {
-      body.style.backgroundColor = e.target.id;
-    }
-  });
-});
+buttons.forEach(function(btnval){
+btnval.addEventListener('click',function(e){
+if(e.target.id === 'grey'){
+    body.style.backgroundColor =e.target.id
+}
+if(e.target.id === 'red'){
+    body.style.backgroundColor =e.target.id
+}
+if(e.target.id === 'blue'){
+    body.style.backgroundColor =e.target.id
+}
+if(e.target.id === 'yellow'){
+    body.style.backgroundColor =e.target.id
+}
+if(e.target.id === 'purple'){
+    body.style.backgroundColor =e.target.id
+}
+
+
+})
+
+
+})
